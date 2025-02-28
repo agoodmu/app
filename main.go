27779612f4277c77b4a2932488e5e1391159cdba
Jimmy Mujianmin <jimmy.mujianmin@globe.com.ph>
2025-02-28 17:59:55 +0800

    update

--- main.go
+++ main.go
@@ -7,45 +7,40 @@ import (
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
 )
 
-// hello is a component that displays a simple "Hello World!". A component is a
-// customizable, independent, and reusable UI element. It is created by
-// embedding app.Compo into a struct.
 type hello struct {
 	app.Compo
+
+	name string
 }
 
-// The Render method is where the component appearance is defined. Here, a
-// "Hello World!" is displayed as a heading.
 func (h *hello) Render() app.UI {
-	return app.H1().Text("Hello World!")
+	return app.Div().Body(
+		app.H1().Body(
+			app.Text("Hello, "),
+			app.If(h.name != "", func() app.UI {
+				return app.Text(h.name)
+			}).Else(func() app.UI {
+				return app.Text("World!")
+			}),
+		),
+		app.P().Body(
+			app.Input().
+				Type("text").
+				Value(h.name).
+				Placeholder("What is your name?").
+				AutoFocus(true).
+				OnChange(h.ValueTo(&h.name)),
+		),
+	)
 }
 
 func main() {
-	// The first thing to do is to associate the hello component with a path.
-	//
-	// This is done by calling the Route() function,  which tells go-app what
-	// component to display for a given path, on both client and server-side.
+	// Go-app component routing (client-side):
 	app.Route("/", func() app.Composer { return &hello{} })
-
-	// Once the routes set up, the next thing to do is to either launch the app
-	// or the server that serves the app.
-	//
-	// When executed on the client-side, the RunWhenOnBrowser() function
-	// launches the app,  starting a loop that listens for app events and
-	// executes client instructions. Since it is a blocking call, the code below
-	// it will never be executed.
-	//
-	// When executed on the server-side, RunWhenOnBrowser() does nothing, which
-	// lets room for server implementation without the need for precompiling
-	// instructions.
+	app.Route("/hello", func() app.Composer { return &hello{} })
 	app.RunWhenOnBrowser()
 
-	// Finally, launching the server that serves the app is done by using the Go
-	// standard HTTP package.
-	//
-	// The Handler is an HTTP handler that serves the client and all its
-	// required resources to make it work into a web browser. Here it is
-	// configured to handle requests with a path that starts with "/".
+	// Standard HTTP routing (server-side):
 	http.Handle("/", &app.Handler{
 		Name:        "Hello",
 		Description: "An Hello World! example",
